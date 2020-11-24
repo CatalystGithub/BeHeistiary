f9dbca4ae5e939504d5e4e14b3a94024065e960c
CatalystGithub <74880451+CatalystGithub@users.noreply.github.com>
2020-11-24 19:58:22 +0300

    Update BeHeistiary.cs

--- BeHeistiary.cs
+++ BeHeistiary.cs
@@ -12,6 +12,11 @@ namespace BeHeistiary
 
             }
 
+            if (ImGui.TreeNodeEx("Bestiary Options", ImGuiTreeNodeFlags.CollapsingHeader))
+            {
+                //Settings.Bestiary.Toggle1.Value = Checkbox("Tree Node for Bestiary", Settings.Bestiary.Toggle1.Value);
+            }
+
             bool Checkbox(string labelString, bool boolValue)
             {
                 ImGui.Checkbox(labelString, ref boolValue);
